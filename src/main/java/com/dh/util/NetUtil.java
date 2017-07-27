008e5fb4a9c1af4ffe5cb587e09dd7814b261520
Xiangyu LI <lxyiiangu@126.com>
2017-07-27 21:10:55 +0800

    c

--- src/main/java/com/dh/util/NetUtil.java
+++ src/main/java/com/dh/util/NetUtil.java
@@ -1,16 +1,10 @@
-/**
- * @(#)NetUtil.java, 2017/7/27.
- * <p/>
- * Copyright 2017 Netease, Inc. All rights reserved.
- * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package com.dh.util;
 
 import java.lang.reflect.AccessibleObject;
 import java.net.InetAddress;
 
 /**
- * @author lixiangyu(hzlixiangyu@corp.netease.com)
+ * @author lixiangyu
  */
 public class NetUtil {
 
